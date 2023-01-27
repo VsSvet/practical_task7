9a9fb6690f4c88df788e905599c15027c60fe399
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-27 16:58:45 +0300

    add Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите размер m: ");
+int M = int.Parse(ReadLine());
+Write("Введите размер n: ");
+int N = int.Parse(ReadLine());
+
+
+double[,] result = new double[M, N];
+
+for(int i = 0; i < M; i++)
+{
+    for(int j = 0; j < N; j++)
+    {
+        result[i, j] = Math.Round(new Random().NextDouble()*10 -1, 1);
+        Write($"{result[i,j]} ");    
+    }
+    WriteLine();
+}
