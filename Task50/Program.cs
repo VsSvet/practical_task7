a43a217221648e42a28160f17212150df9bd1187
Vostrova Svetlana <masuanuamasuanua1@gmail.com>
2023-01-27 17:56:29 +0300

    Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите номер столбца: ");
+int column = int.Parse(ReadLine());
+Write("Введите номер строки: ");
+int row = int.Parse(ReadLine());
+
+int[,] matrix = {{1, 4, 7, 2}, {5, 9, 3, 2}, {8, 4, 2, 4}};
+
+if (column < 3 & row < 4)
+{
+    WriteLine(matrix[column, row]);
+}
+else
+{
+    WriteLine($"{column}{row} такого числа в массиве нет");
+}
